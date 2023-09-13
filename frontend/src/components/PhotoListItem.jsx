9cbc8068f2e2f343a0923e3f5e8fe0cda313165f
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-09-13 11:27:09 -0400

    Refactor PhotoList to show different photos

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,14 +10,14 @@ const PhotoListItem = (props) => {
     <section className="photo-list__item">
       <div>
         <PhotoFavButton />
-      <img className="photo-list__image" src={props.imageSource} />
+      <img className="photo-list__image" src={props.urls.regular} />
       </div>
       <div className="photo-list__user-details">
         <div>
-        <img className="photo-list__user-profile" src={props.profile} />
+        <img className="photo-list__user-profile" src={props.user.profile} />
         </div>
         <div className="photo-list__user-info">
-          {props.username}
+          {props.user.name}
           <div className="photo-list__user-location">
           {`${props.location.city}, ${props.location.country}`}
           </div>
