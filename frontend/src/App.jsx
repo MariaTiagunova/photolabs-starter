6e1ab1a0e4a1beec30115d6f4bbbcf79badef45a
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-09-13 14:38:38 -0400

    Add a notification for favourited photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <HomeRoute topics = {topics} photos = {photos} favorites={favorites} toggleFavorite={toggleFavorite} />
+      <HomeRoute topics = {topics} photos = {photos} favorites={favorites} toggleFavorite={toggleFavorite} isFavPhotoExist={(favorites.length > 0)}/>
       
     </div>
   );
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -7,7 +7,7 @@ import PhotoList from 'components/PhotoList';
 const HomeRoute = (props) => {
   return (
     <div className="home-route">
-      <TopNavigation topics = {props.topics} />
+      <TopNavigation topics = {props.topics} isFavPhotoExist={props.isFavPhotoExist}/>
       <PhotoList photos =  {props.photos} favorites={props.favorites} toggleFavorite={props.toggleFavorite}/>
     </div>
   );
