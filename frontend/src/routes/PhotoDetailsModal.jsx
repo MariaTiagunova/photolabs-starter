c90150e00dfee02ec537e5d1fb7ae6a0600dcc7a
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-09-17 13:05:53 -0400

    Add fav button to modal main photo

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,12 +3,15 @@ import '../styles/PhotoDetailsModal.scss';
 import '../styles/PhotoListItem.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from 'components/PhotoList';
+import PhotoFavButton from 'components/PhotoFavButton';
 
 const PhotoDetailsModal = (props) => {
   const { urls, user, location } = props.photo;
   const similarPhotos = props.similarPhotos;
   // Convert similar_photos object values into an array
   const similarPhotosArray = Object.values(similarPhotos);
+  // Check if props.favorites is defined and contains the id
+  const isFavorite = props.favorites && props.favorites.includes(props.photo.id);
 
   return (
     <div className="photo-details-modal">
@@ -19,6 +22,10 @@ const PhotoDetailsModal = (props) => {
         <img src={closeSymbol} alt="close symbol" />
       </button>
       <section className='photo-details-modal__images'>
+      <PhotoFavButton 
+        photoId = {props.photo.id}
+        isFavorite={isFavorite}
+        toggleFavorite = {props.toggleFavorite} />
         <img className='photo-details-modal__image' src={urls.full} />
         <div className='photo-details-modal__photographer-details'>
           <img className="photo-list__user-profile" src={user.profile} />
